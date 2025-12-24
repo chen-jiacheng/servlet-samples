cfbcd8bdab950fb4e0fac740bfba5ff39aa7e88c
chenjiacheng <chen-jiacheng@foxmail.com>
2025-12-24 23:53:15 +0800

    新增cors配置

--- src/main/java/com/chenjiacheng/webapp/servletsamples/servlet/filter/CorsFilter.java
+++ src/main/java/com/chenjiacheng/webapp/servletsamples/servlet/filter/CorsFilter.java
@@ -17,7 +17,7 @@ public class CorsFilter implements Filter {
         HttpServletResponse httpResponse = (HttpServletResponse) response;
         
         // 设置CORS头
-        httpResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        httpResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:8081");
         httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
         httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
